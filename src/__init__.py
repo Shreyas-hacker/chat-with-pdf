3cd11ef6df7c43f33d6ebede9226ad8db9a9e2a9
davidchen <daviddhc20120601@gmail.com>
2023-09-04 15:02:08 +0800

    13b

--- src/__init__.py
+++ src/__init__.py
@@ -13,35 +13,6 @@ def create_app(config_name):
                        page_icon='🤖', layout='centered', initial_sidebar_state='auto')
     st.header(cf.TITLE)
 
-    open_api_token_global = "something"
-    # open_api_token_global = st.text_input('your openai token', 'something')
-    # st.write('The current chatgpt api token is', open_api_token_global)
-
-    #  adbpg_user_input, adbpg_pwd_input
-    # # Returns `None` if the key doesn't exist
-    # print(os.environ.get('KEY_THAT_MIGHT_EXIST'))
-    # conn_string = os.environ.get('PG_HOST', '')
-    # conn_port = os.environ.get('PG_PORT', '')
-    # adbpg_database = os.environ.get('PG_DATABASE', '')
-    # adbpg_user = os.environ.get('PG_USER', '')
-    # adbpg_passwd = os.environ.get('PG_PASSWORD', '')
-
-    # adbpg_host_input_global = ""
-    # adbpg_port_input_global = ""
-    # adbpg_database_input_global = ""
-    # adbpg_user_input_global = ""
-    # adbpg_pwd_input_global = ""
-
-    # adbpg_host_input_global = st.text_input('your adbpg_host', conn_string)
-    # st.write('The current adbpg_host_input', adbpg_host_input_global)
-    # adbpg_port_input_global = st.text_input('your adbpg_port', conn_port)
-    # st.write('The current adbpg_port is', adbpg_port_input_global)
-    # adbpg_database_input_global = st.text_input('your adbpg_database', adbpg_database)
-    # st.write('The current adbpg_database is', adbpg_database_input_global)
-    # adbpg_user_input_global = st.text_input('your adbpg_user', adbpg_user)
-    # st.write('The current adbpg_user is', adbpg_user_input_global)
-    # adbpg_pwd_input_global = st.text_input('your adbpg_pwd', adbpg_passwd)
-    # st.write('The current adbpg_pwd is', adbpg_pwd_input_global)
 
     # we need a way to remember the chat history
     if 'user_input' not in st.session_state:
@@ -62,13 +33,6 @@ def create_app(config_name):
         st.session_state.user_input = st.session_state.widget
         st.session_state.widget = ''
 
-    # def show_pdf(file_path):
-    #     with open(file_path, "rb") as f:
-    #         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    #         pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width={cf.WIDTH} ' \
-    #                       f'height={cf.HEIGHT} type="application/pdf"></iframe>'
-    #         st.markdown(pdf_display, unsafe_allow_html=True)
-
     def excerpt(text, n=cf.EXCERPT_LENGTH):
         return text[:n] + '...' if len(text) > n else text
     """
@@ -76,36 +40,17 @@ def create_app(config_name):
     *---------------------------------------*-----------------------------------------------------*
     """
 
-    # let the user upload a pdf or txt file
-    # pdf = st.file_uploader(
-    #     'Upload a PDF file',
-    #     type=[ext for ext in cf.ALLOWED_FILE_EXTENSION.split(',')],
-    #     accept_multiple_files=cf.ALLOW_MULTIPLE_FILES,
-    #     label_visibility='hidden'
-    # )
+
     if True:
         # show the pdf thumbnail :-)
 
         response_container = st.container()
-        # processing start here...
-        # s = setup(file=fp, number_of_relevant_chunk=cf.NUMBER_OF_RELEVANT_CHUNKS, open_ai_token=open_api_token_global,
-        #           adbpg_host_input=adbpg_host_input_global, adbpg_port_input = adbpg_port_input_global,
-        #           adbpg_database_input=adbpg_database_input_global, adbpg_user_input=adbpg_user_input_global, adbpg_pwd_input=adbpg_pwd_input_global)
+
         st.text_input('🤖 666 👋🏾, Ask me anything', key='widget', on_change=submit)
         with response_container:
             if st.session_state.user_input:
                 response = generate_response(st.session_state.user_input)
-                if response['source_documents']:
-                    all_refs = ''
-                    for doc in response['source_documents']:
-                        content = excerpt(doc.page_content)
-                        page = doc.metadata.get('page')
-                        ref = f"""
-                        *{content}*
-                        \n☝🏽#{page} 📖\n
-                        """
-                        all_refs += ref
-                    st.session_state.source.append(all_refs)
+
                 st.session_state.past.append(st.session_state.user_input)
                 st.session_state.generated.append(response['answer'])
             if st.session_state.generated:
--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -1,25 +1,10 @@
 from __future__ import annotations
-import os
+
 import pprint
 from tempfile import NamedTemporaryFile
 from typing import Any
 import streamlit as st
-from langchain import OpenAI
-from langchain.chains import RetrievalQA
-from langchain.chains import ConversationalRetrievalChain
-from langchain.document_loaders import PyPDFLoader
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.schema import Document
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import Chroma
-from langchain.vectorstores.base import VectorStoreRetriever
-# from langchain.llms import Tongyi
-
-import psycopg2cffi
-from langchain.vectorstores.analyticdb import AnalyticDB
-
-from init_llama2_medium import llmLlama2, embeddingsllama2
-from src.helpers.Analyticdbhaidong import AnalyticDBhaidong
+from init_llama2_13b_fast import model
 
 my_openai_api_key = 'something'
 
@@ -33,79 +18,22 @@ def get_text():
     return input_text
 
 
-def generate_response(query: str, chain_type: str, retriever: VectorStoreRetriever, open_ai_token) -> dict[str, Any]:
+def generate_response(query: str) -> dict[str, Any]:
 
     if query == "who are you":
         result_content = """I am HD C """
-        result = {'query': query, 'result': result_content, 'source_documents': [
-            Document(
-                page_content='alibaba cloud SA team hd C ',
-                metadata={'source': '/tmp/tmplvqwt_4h.pdf', 'page': 7}),
-            Document(
-                page_content='super hero SA HD C',
-                metadata={'source': '/tmp/tmplvqwt_4h.pdf', 'page': 9}),
-
-        ]}
+        result = {'query': query, 'result': result_content}
         return result
     else:
-        # qa = RetrievalQA.from_chain_type(
-        #     # llm=OpenAI(openai_api_key = open_ai_token, model_name="gpt-3.5-turbo-16k"),
-        #     llm=llmLlama2,
-        #     chain_type=chain_type,
-        #     retriever=retriever,
-        #     return_source_documents=True
-        # )
-        # result = qa({'query': query})
-        # pprint.pprint(result)
+
         chat_history = []
-        qa = ConversationalRetrievalChain.from_llm(llmLlama2, retriever, return_source_documents=True)
-        # query = "who is david haidong chen"
-        result = qa({"question": query, "chat_history": chat_history})
+
+        result = model(query, stream=False)
         pprint.pprint(result)
 
     return result
 
 
-def transform_document_into_chunks(document: list[Document]) -> list[Document]:
-    """Transform document into chunks of {1000} tokens"""
-    splitter = CharacterTextSplitter(
-        chunk_size=int(os.environ.get('CHUNK_SIZE', 1000)),
-        chunk_overlap=int(os.environ.get('CHUNK_OVERLAP', 0))
-    )
-    return splitter.split_documents(document)
 
 
 
-
-# def transform_chunks_into_embeddings(text: list[Document], k: int , open_ai_token , adbpg_host_input, adbpg_port_input, adbpg_database_input, adbpg_user_input, adbpg_pwd_input) -> VectorStoreRetriever:
-#     """Transform chunks into embeddings"""
-#     CONNECTION_STRING = AnalyticDBhaidong.connection_string_from_db_params(
-#         driver=os.environ.get("PG_DRIVER", "psycopg2cffi"),
-#         host=os.environ.get("PG_HOST", adbpg_host_input),
-#         port=int(os.environ.get("PG_PORT", adbpg_port_input)),
-#         database=os.environ.get("PG_DATABASE", adbpg_database_input),
-#         user=os.environ.get("PG_USER", adbpg_user_input),
-#         password=os.environ.get("PG_PASSWORD", adbpg_pwd_input),
-#     )
-#
-#     # embeddings = OpenAIEmbeddings(openai_api_key = open_ai_token)
-#     embeddings = embeddingsllama2
-#
-#     db = AnalyticDBhaidong.from_documents(text, embeddings, connection_string=CONNECTION_STRING)
-#     return db.as_retriever(search_type='similarity', search_kwargs={'k': k})
-
-def get_file_path(file) -> str:
-    """Obtain the file full path."""
-    with NamedTemporaryFile(dir='/tmp/', suffix='.pdf', delete=False) as f:
-        f.write(file.getbuffer())
-        return f.name
-
-
-def setup(file: str, number_of_relevant_chunk: int, open_ai_token: str , adbpg_host_input, adbpg_port_input, adbpg_database_input, adbpg_user_input, adbpg_pwd_input) -> VectorStoreRetriever:
-    # load the document
-    loader = PyPDFLoader(file)
-    document = loader.load()
-    # transform the document into chunks
-    chunks = transform_document_into_chunks(document)
-    # transform the chunks into embeddings
-    return transform_chunks_into_embeddings(chunks, number_of_relevant_chunk ,open_ai_token,adbpg_host_input, adbpg_port_input, adbpg_database_input, adbpg_user_input, adbpg_pwd_input)
