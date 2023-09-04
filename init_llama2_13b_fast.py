78a8227b4766ce3cf8990365a990bf7b997c857a
davidchen <daviddhc20120601@gmail.com>
2023-09-04 11:32:03 +0800

    13b

--- init_llama2_13b_fast.py
+++ init_llama2_13b_fast.py
@@ -1,7 +1,7 @@
 
 from torch import cuda, bfloat16
 
-model_id = 'OpenAssistant/llama2-13b-orca-8k-3319' # daryl149/llama-2-7b-chat-hf working but half break the vectorstore LinkSoul/Chinese-Llama-2-7b NousResearch/Nous-Hermes-llama-2-7b
+model_id = 'togethercomputer/LLaMA-2-7B-32K' # daryl149/llama-2-7b-chat-hf working but half break the vectorstore LinkSoul/Chinese-Llama-2-7b NousResearch/Nous-Hermes-llama-2-7b
 # full size workding model : NousResearch/Nous-Hermes-llama-2-7b
 device = f'cuda:{cuda.current_device()}' if cuda.is_available() else 'cpu'
 
@@ -27,3 +27,17 @@ The title should not exceed 20 words. Descriptions can be based on input text wi
 
 print(model(prompt_001, stream=False))
 
+
+###############
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("OpenAssistant/llama2-13b-orca-8k-3319", use_fast=False)
+model = AutoModelForCausalLM.from_pretrained("OpenAssistant/llama2-13b-orca-8k-3319", torch_dtype=torch.float16, low_cpu_mem_usage=True, device_map="auto")
+
+system_message = "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."
+user_prompt = "Write me a poem please"
+prompt = f"""<|system|>{system_message}</s><|prompter|>{user_prompt}</s><|assistant|>"""
+inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
+output = model.generate(**inputs, do_sample=True, top_p=0.95, top_k=0, max_new_tokens=256)
+print(tokenizer.decode(output[0], skip_special_tokens=True))
