e446dc7018c1027e485c9e02d7d55f4a30309a6a
davidchen <daviddhc20120601@gmail.com>
2023-09-17 16:33:12 +0800

    fingpt init

--- init_llama2_medium.py
+++ init_llama2_medium.py
@@ -13,30 +13,33 @@ model = torch.compile(model)  # Please comment this line if your platform does n
 model = model.eval()
 
 # Make prompts
-prompt = [
-'''Instruction: What is the sentiment of this news? Please choose an answer from {negative/neutral/positive}
+# prompt = [
+# '''Instruction: What is the sentiment of this news? Please choose an answer from {negative/neutral/positive}
+# Input: FINANCING OF ASPOCOMP 'S GROWTH Aspocomp is aggressively pursuing its growth strategy by increasingly focusing on technologically more demanding HDI printed circuit boards PCBs .
+# Answer: ''',
+# '''Instruction: What is the sentiment of this news? Please choose an answer from {negative/neutral/positive}
+# Input: According to Gran , the company has no plans to move all production to Russia , although that is where the company is growing .
+# Answer: ''',
+# '''Instruction: What is the sentiment of this news? Please choose an answer from {negative/neutral/positive}
+# Input: A tinyurl link takes users to a scamming site promising that users can earn thousands of dollars by becoming a Google ( NASDAQ : GOOG ) Cash advertiser .
+# Answer: ''',
+# ]
+prompt = '''Instruction: What is the sentiment of this news? Please choose an answer from {negative/neutral/positive}
 Input: FINANCING OF ASPOCOMP 'S GROWTH Aspocomp is aggressively pursuing its growth strategy by increasingly focusing on technologically more demanding HDI printed circuit boards PCBs .
-Answer: ''',
-'''Instruction: What is the sentiment of this news? Please choose an answer from {negative/neutral/positive}
-Input: According to Gran , the company has no plans to move all production to Russia , although that is where the company is growing .
-Answer: ''',
-'''Instruction: What is the sentiment of this news? Please choose an answer from {negative/neutral/positive}
-Input: A tinyurl link takes users to a scamming site promising that users can earn thousands of dollars by becoming a Google ( NASDAQ : GOOG ) Cash advertiser .
-Answer: ''',
-]
+Answer: '''
 
 # Generate results
 tokens = tokenizer(prompt, return_tensors='pt', padding=True, max_length=512)
 res = model.generate(**tokens, max_length=512)
-res_sentences = [tokenizer.decode(i) for i in res]
-out_text = [o.split("Answer: ")[1] for o in res_sentences]
+# res_sentences = [tokenizer.decode(i) for i in res]
+res_sentence = tokenizer.decode(res)
+out_text = res_sentence.split("Answer: ")[1]
 
 # show results
 for sentiment in out_text:
     print(sentiment)
 
-for result_sent in res_sentences:
-    print(result_sent)
+print(res_sentence)
 # print(res_sentences)
 # print("res"+res)
 
